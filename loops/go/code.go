b831bce0209daea7597a5c2114317e1989336772
Hiroaki Nakamura <hnakamur@gmail.com>
2024-12-25 09:56:32 +0900

    Measure rand and loop seperately in Go loop benchmark
    ```
    $ cd loops/
    $ go build -ldflags "-s -w" -o go/code go/code.go
    $ time ./go/code 40
    1951140
    rand=4.585µs, loop=1.272208584s
    real    0m1.280s
    user    0m1.273s
    sys     0m0.008s
    $ cat input.txt
    40
    ```

--- loops/go/code.go
+++ loops/go/code.go
@@ -1,22 +1,30 @@
 package main
+
 import (
-    "fmt"
-    "math/rand"
-    "strconv"
-    "os"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-  input, e := strconv.Atoi(os.Args[1]) // Get an input number from the command line
-  if e != nil { panic(e) }
-  u := int32(input)
-  r := rand.Int31n(10000)              // Get a random number 0 <= r < 10k
-  var a[10000]int32                      // Array of 10k elements initialized to 0
-  for i := int32(0); i < 10000; i++ {         // 10k outer loop iterations
-    for j := int32(0); j < 100000; j++ {      // 100k inner loop iterations, per outer loop iteration
-      a[i] = a[i] + j%u                // Simple sum
-    }
-    a[i] += r                          // Add a random value to each element in array
-  }
-  fmt.Println(a[r])                    // Print out a single element from the array
+	input, e := strconv.Atoi(os.Args[1]) // Get an input number from the command line
+	if e != nil {
+		panic(e)
+	}
+	u := int32(input)
+	t1 := time.Now()
+	r := rand.Int31n(10000) // Get a random number 0 <= r < 10k
+	t2 := time.Now()
+	var a [10000]int32                  // Array of 10k elements initialized to 0
+	for i := int32(0); i < 10000; i++ { // 10k outer loop iterations
+		for j := int32(0); j < 100000; j++ { // 100k inner loop iterations, per outer loop iteration
+			a[i] = a[i] + j%u // Simple sum
+		}
+		a[i] += r // Add a random value to each element in array
+	}
+	t3 := time.Now()
+	fmt.Println(a[r]) // Print out a single element from the array
+	fmt.Printf("rand=%s, loop=%s", t2.Sub(t1), t3.Sub(t2))
 }
